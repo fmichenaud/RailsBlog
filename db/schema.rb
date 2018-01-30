0e6ec0051c5cbf1f285d26f3ad4499b719979706
fmichenaud <florent.michenaud@gmail.com>
2018-01-30 10:41:59 +0100

    creating articles controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130084902) do
+ActiveRecord::Schema.define(version: 20180130090015) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.date "published"
+    t.text "heading"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
