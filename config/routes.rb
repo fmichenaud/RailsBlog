0cd428ca5d2e333fb64db7b10e91dda43e0a5f6f
fmichenaud <florent.michenaud@gmail.com>
2018-02-03 17:53:01 +0100

    adding users registration and login, with a name field overloaded to the devise class

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: :registrations}
   get 'home/index'
 
   resources :articles
+  resources :users
   
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
