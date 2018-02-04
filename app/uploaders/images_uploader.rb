9c98c3281933b0f69ee456387633dc871b7556c9
fmichenaud <florent.michenaud@gmail.com>
2018-02-04 02:29:23 +0100

    starting layout of the index page and register form

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -33,6 +33,10 @@ version :thumb do
   process resize_to_fit: [50, 50]
 end
 
+version :small do
+  process resize_to_fit: [400, 400]
+end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
 def extension_whitelist
